b9b129b485af4a14cf6c7014853c7d110ece6cbe
jongholee <jhleed3@gmail.com>
2019-04-21 14:03:10 +0900

    Add Test Sample & Install Mocha

---
+++ test/test-sample.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('Test 1', function () {
+    it('Test Case 1', function () {
+        assert.strictEqual(1, 1);
+    });
+
+    it('Test Case 2', function () {
+        assert.strictEqual(1, 1);
+    });
+});
