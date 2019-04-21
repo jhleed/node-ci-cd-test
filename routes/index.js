46c670cdc1d69bb95f15fafe8a654d03ad368d6c
jongholee <jhleed3@gmail.com>
2019-04-21 15:51:48 +0900

    Refactor Index Route, Controller

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const indexController = require('../controller/indexController');
 
-router.get('/', (req, res, next) => indexController.index()
-    .then(input => res.render('index', input))
-);
+router.get('/', indexController.index);
 
 router.get('/about', (req, res, next) => res.send('created from Jongho Lee'));
 
