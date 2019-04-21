aa48d0617d9d02116fc00cc2b8b2a2dc72a809c4
jongholee <jhleed3@gmail.com>
2019-04-21 21:33:52 +0900

    Sequelize 연동 성공

--- persistence/userRepository.js
+++ persistence/userRepository.js
@@ -1,33 +1,7 @@
-const Q = require('q');
 const User = require('../models').User;
 
 function getUsers() {
-
-    let deferred = Q.defer();
-
-    // const category = User.findAll({});
-
-    // deferred.resolve(category);
-
-    setTimeout(() => {
-
-        // const category = User.findAll({});
-        //
-        // deferred.resolve(category);
-
-        deferred.resolve([
-            {
-                id: 1,
-                name: '이종호'
-            },
-            {
-                id: 2,
-                name: '임퍼뚱'
-            }
-        ]);
-    }, 500);
-
-    return deferred.promise;
+    return User.findAll();
 }
 
 module.exports = {
--- test/src/integrationTest.js
+++ test/src/integrationTest.js
@@ -4,16 +4,8 @@ const userRepository = require('../../persistence/userRepository');
 describe('userRepository', () => {
     it('should return 2 elements when call userCategories', function (done) {
         userRepository.getUsers().then((val) => {
-            assert.deepStrictEqual(val, [
-                {
-                    id: 1,
-                    name: '이종호'
-                },
-                {
-                    id: 2,
-                    name: '임퍼뚱'
-                }
-            ]);
+            assert.strictEqual(val[0].id, 1);
+            assert.strictEqual(val[0].name, '이종호');
             done();
         })
     });
